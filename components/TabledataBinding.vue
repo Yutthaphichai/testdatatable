cb59f981d53ba196d5f0ed6b2f69489b019ea2a4
Yutthaphichai <41717639+Yutthaphichai@users.noreply.github.com>
2022-06-09 18:14:14 +0700

    testdata

---
+++ components/TabledataBinding.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app-container">
+    <DxDataGrid :data-source="employees" key-expr="EmployeeID">
+      <DxColumn data-field="FullName"></DxColumn>
+      <DxColumn data-field="Position"></DxColumn>
+      <DxColumn data-field="BirthDate" data-type="date">
+      </DxColumn>
+      <DxColumn data-field="HireDate" data-type="date">
+      </DxColumn>
+      <DxColumn data-field="City" />
+      <DxColumn data-field="Country"></DxColumn>
+      <DxColumn data-field="Address" />
+      <DxColumn data-field="HomePhone" />
+      <DxColumn data-field="PostalCode" />
+    </DxDataGrid>
+  </div>
+</template>
+ 
+<script>
+// ...
+
+import {
+  DxDataGrid,
+  DxColumn,
+  DxGrouping,
+  DxGroupPanel,
+  DxPager,
+  DxPaging,
+  DxSearchPanel,
+} from 'devextreme-vue/data-grid';
+import service from './employees.service';
+
+export default {
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxGrouping,
+    DxGroupPanel,
+    DxPager,
+    DxPaging,
+    DxSearchPanel,
+    // DiscountCell,
+  },
+  data() {
+    return {
+      employees: service.getEmployees(),
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <div class="mb-10 ml-10 mr-10 mt-10">
+      <p class="text-center text-green-600">Tabledata </p>
+      <Table />
+    </div> -->
+    <div class="mb-10 ml-10 mr-10 mt-10">
+      <p class="text-center text-green-600">Tabledata Binding</p>
+      <TabledataBinding />
+    </div>
+  </div>
+
+</template>
+
+<script>
+// import 'devextreme/dist/css/dx.light.css';
+export default {
+  name: 'IndexPage'
+}
+</script>
